0d5a0af876926dc747c9d0526de79bdb89c3b8cd
pedroevaristo <pedrodzprogramacao@gmail.com>
2024-09-17 18:12:57 -0300

    começando a fazer a parte inicial do acesso e cadastro, e organizando part 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite seu acesso \n Não tem ? Cadastre agora");
+
+    }
+}
